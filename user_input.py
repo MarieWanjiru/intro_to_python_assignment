a6dcc09a4c1f6772aea327fc5b0ee42940286132
MarieWanjiru <160642947+MarieWanjiru@users.noreply.github.com>
2024-02-26 05:36:47 -0800

    Add files via upload
    my first commit

---
+++ user_input.py
@@ -0,0 +1,7 @@
+# Collecting the information
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+
+# Displaying the results
+print(f"Hello{name}, you are{age} years old and live in{location}")
